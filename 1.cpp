9548c9a70b37b8df38fc4c853f603015ea1f1819
BenjaminSong <1078412242@qq.com>
2020-12-02 16:30:15 +0900

    2020-12-02

---
+++ 1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class Geeks
+{
+    // Access Specifier
+public:
+    // Data Members
+    string geekName;
+
+    // member functions
+    void printName()
+    {
+        cout << "Geek name is : " << geekName;
+    }
+};
+
+int main(){
+    
+    // instance
+}
